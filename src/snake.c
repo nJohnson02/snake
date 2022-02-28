d0935130abb4b36ca8259e74fbe6b3869b7a326a
dapp3rduck <n.johnson01@protonmail.ch>
2022-02-28 15:50:54 -0700

    yes

--- src/snake.c
+++ src/snake.c
@@ -1,7 +1,45 @@
-#include <stdio.h>
+/*
 
+Create the snake [array of coords]
+Max length of snake = area of board
+
+Create a length variable
+Increment length every time the snake grows
+
+Pick an empty space for food every time the snake grows
+
+Every frame:
+    check the last key press
+    move snake in direction of keypress
+    wait
+
+
+
+  0 1 2 3 4 5 6 7 8 9
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+{{5,5},{5,4}}
+
+*/
+
+#include <stdio.h>
 int main() {
     
+    int rows = 10;
+    int cols = 10;
+
+    int snake[rows*cols][2];
+
     
+
     return 0;
 }
