440d5f420d614588ec9ba829f0813562e1282de1
dapp3rduck <n.johnson01@protonmail.ch>
2022-03-23 15:53:01 -0700

    sort of working :)

--- src/snake.c
+++ src/snake.c
@@ -132,10 +132,9 @@ int main() {
       }
 
       // Move
-      for (int i = score-1; i < -1; i--) {
+      for (int i = score-1; i >= 1; i--) {
         snake[i][0] = snake[i-1][0];
         snake[i][1] = snake[i-1][1];
-
       }
 
       // Update apple
@@ -154,7 +153,7 @@ int main() {
 
       // Update grid
       for (int i = 0; i < ROWS*COLS; i++) {
-        if (snake[i][0] > 0 && snake[i][1] > 0)
+        if (snake[i][0] >= 0 && snake[i][1] >= 0)
           grid[snake[i][0]][snake[i][1]] = 178;
       }
 
