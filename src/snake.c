7500d7048b13074b3829a990b71be4cb86b63972
dapp3rduck <n.johnson01@protonmail.ch>
2022-03-24 14:35:50 -0700

    no turn around

--- src/snake.c
+++ src/snake.c
@@ -5,10 +5,11 @@
 #include <winuser.h>
 
 
-#define ROWS 10
-#define COLS 30
+#define ROWS 20
+#define COLS 20
 
 //how to make colored text: https://www.theurbanpenguin.com/4184-2/
+//ascii table https://www.keepandshare.com/userpics/r/o/b/e/rt/2019-12/sb/screen_shot_2019_12_01_at_3.26.20_pm-34867850.jpg?ts=1575242835
 
 void colorRed() {
   printf("\033[0;31m");
@@ -56,8 +57,7 @@ void drawFrame(unsigned char frame[ROWS][COLS]) {
       else
         colorGreen();
 
-      printf("%c", frame[i][j]);
-      printf("%c", frame[i][j]);
+      printf("%c%c", frame[i][j], frame[i][j]);
 
     }
 
@@ -81,7 +81,7 @@ int main() {
     unsigned char grid[ROWS][COLS];
     int snake[ROWS*COLS][2];
     char keyPress;
-    char key;
+    char key = 'd';
     int score = 1;
     int apple[2];
     int alive = 1;
@@ -95,8 +95,8 @@ int main() {
     }
 
     // Set starting position
-    snake[0][0] = 5;
-    snake[0][1] = 5;
+    snake[0][0] = ROWS/2-1;
+    snake[0][1] = COLS/2-1;
 
     // Set Starting Apple
     apple[0] = rand()%ROWS;
@@ -107,17 +107,38 @@ int main() {
       
       Sleep(300);     
 
+
       // Move
       for (int i = score-1; i >= 1; i--) {
         snake[i][0] = snake[i-1][0];
         snake[i][1] = snake[i-1][1];
       }
 
+
       // Check keyboard
       keyPress = checkKb();
-      if (keyPress != '\0') 
-        key = keyPress;
       
+      if (keyPress != '\0') { 
+        switch (keyPress) {
+        case 'w':
+          if (key!='s')
+            key = keyPress;
+          break;
+        case 'a':
+          if (key!='d')
+            key = keyPress;
+          break;
+        case 's':
+          if (key!='w')
+            key = keyPress;
+          break;
+        case 'd':
+          if (key!='a')
+            key = keyPress;
+          break;
+        }
+      }
+
       switch (key) {
         case 'w':
           snake[0][0]--;
@@ -128,17 +149,19 @@ int main() {
         case 's':
           snake[0][0]++;
           break;
-        default:
+        case 'd':
           snake[0][1]++;
           break;
       }
 
+
       // Eat apple
-      if (grid[apple[0]][apple[1]] == grid[snake[0][0]][snake[0][1]]) {
+      if (apple[0] == snake[0][0] && apple[1] == snake[0][1]) {
         apple[0] = rand()%ROWS;
         apple[1] = rand()%COLS;
         score++;
       }
+
 
       // Check for death
       for (int i = 1; i < ROWS*COLS; i++) {
@@ -160,6 +183,7 @@ int main() {
         }
       }
 
+
       // Update grid
       for (int i = 0; i < ROWS*COLS; i++) {
         if (snake[i][0] >= 0 && snake[i][1] >= 0)
@@ -168,6 +192,7 @@ int main() {
 
       grid[apple[0]][apple[1]] = 219;
       drawFrame(grid);
+
 
       //Print Score
       printf("Size: %d\n", score);
